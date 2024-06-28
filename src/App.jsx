90faa521c060bf0ad4bf660ffb0caf26d695c68f
Madison Zeiger <addiebellzeiger@gmail.com>
2024-06-28 14:49:51 -0500

    Attemped Homework Completion for React useState Exercise

--- src/App.jsx
+++ src/App.jsx
@@ -4,21 +4,8 @@ import HomePage from "./components/HomePage";
 function App() {
   return (
     <>
-      <nav>
-        <ul>
-          <li> 
-            <a href="#">Home</a>
-          </li>
-          <li>
-            <a href="#">Movies</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-        </ul>
-      </nav>
       <main>
-        <HomePage/>
+        <HomePage />
       </main>
     </>
   );
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,28 +1,45 @@
-import "./HomePage.css"
+import "./HomePage.css";
+import { useState } from "react";
 
 function HomePage() {
+  const [movies, setMovies] = useState([]);
+  const [newMovie, setNewMovie] = useState("");
+
+  const handleInputChange = (e) => {
+    setNewMovie(e.target.value);
+  };
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    setMovies([...movies, newMovie]);
+    setNewMovie(``);
+  };
+
+  const handleRemoveMovie = (index) => {
+    setMovies(movies.filter((_, i) => i !== index));
+  };
+
   return (
     <div>
       <h1>Movie Watchlist</h1>
-      <form>
+      <form onSubmit={handleFormSubmit}>
         <label htmlFor="movieInput">Add a Movie</label>
-        <input type="text" id="movieInput" name="movieInput" />
+        <input
+          type="text"
+          id="movieInput"
+          name="movieInput"
+          onChange={handleInputChange}
+        />
         <button type="submit">Add Movie</button>
+
+        <ul id="watchList">
+          {movies.map((movie, index) => (
+            <li key={index}>
+              {movie}
+              <button onClick={() => handleRemoveMovie(index)}>Remove</button>
+            </li>
+          ))}
+        </ul>
       </form>
-      <ul id="watchList">
-        <li>
-            <p>Harry Potter</p>
-        </li>
-        <li>
-            <p>Lord of the Rings</p>
-        </li>
-        <li>
-            <p>About Time</p>
-        </li>
-        <li>
-            <p>Midnight in Paris</p>
-        </li>
-      </ul>
     </div>
   );
 }
